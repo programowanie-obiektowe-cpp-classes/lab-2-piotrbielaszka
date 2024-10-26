07dbc0a837a31185e37393861eb06776954f83d0
Piotr Bielaszka <piotr.bielaszka@gmail.com>
2024-10-26 21:59:40 +0200

    zrobione

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,31 @@
 
 #include "Resource.hpp"
 
+using namespace std;
+
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    unique_ptr< Resource > res;
+
+public:
+    ResourceManager() : res(make_unique< Resource >()) {}
+    ResourceManager(const ResourceManager& res2) : res(make_unique< Resource >(*res2.res)) {}
+    ResourceManager(ResourceManager&& res2) : res(move(res2.res)) {}
+    ~ResourceManager() {}
+    ResourceManager& operator=(const ResourceManager& res2)
+    {
+        if (this != &res2) {
+            res = make_unique< Resource >(*res2.res);
+        }
+        return *this;
+    }
+    ResourceManager& operator=(ResourceManager&& res2)
+    {
+        if (this != &res2) {
+            res = move(res2.res);
+        }
+        return *this;
+    }
+    double get() { return res->get(); }
 };
